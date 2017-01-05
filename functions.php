ccf4473a258de59ab70fdd97d769f22d8f4d8125
clydus24 <johnbullecer@gmail.com>
2017-01-05 18:16:52 +0800

    enqueue sunvalley-wc.css

--- functions.php
+++ functions.php
@@ -24,15 +24,11 @@ function enqueue_parent_styles() {
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 
 function enqueue_child_styles(){
-//FIRST
-	//wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/css/styles.css' );
-	//wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/css/custom.css' );
 
-	//...custom queueing of .js and .css for Javascript plugins and such here
-
-	//LAST - override css
-	//wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/css/styles.css', array( 'parent-style' ) );
-	//wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/css/custom.css', array( 'parent-style' ) );
+	//register custom css
+	wp_register_style( 'sunvalley-wc.css', get_stylesheet_directory_uri() . '/css/sunvalley-wc.css');
+	
+	//override css - enqueue styles	
 	wp_enqueue_style( 'styles.css', get_stylesheet_directory_uri() . '/css/styles.css');
 	wp_enqueue_style( 'custom.css', get_stylesheet_directory_uri() . '/css/custom.css');
 }
